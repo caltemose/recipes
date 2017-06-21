976ca2ebeaa3dbb7c7197e19e555dc0655db3556
Chad Altemose <chad@chadzilla.com>
2017-06-21 07:35:45 -0400

    added mongo test template

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const fs = require('fs')
 const del = require('del')
+const gulpData = require('gulp-data')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
@@ -8,6 +9,7 @@ const browsersync = require('browser-sync').create()
 const sasslint = require('gulp-sass-lint')
 const cleancss = require('gulp-clean-css')
 const rename = require('gulp-rename')
+const MongoClient = require('mongodb').MongoClient
 
 /* Helper functions
 ---------------------------------------------------------------- */
@@ -16,6 +18,36 @@ const rename = require('gulp-rename')
 //     return JSON.parse(fs.readFileSync('src/data/recipes.json', 'utf-8'))
 // }
 
+var recipesFromDb
+console.log('pre mongo code')
+gulp.task('db', () => {
+    // MongoClient.connect('mongodb://127.0.0.1:27017/foodsys1', function(err, db) {
+    //     if (err) {
+    //         db.close()
+    //         return err
+    //     }
+
+    //     // cb(undefined, db.collection('recipes').find({}));
+    //     db.collection('recipes').find().toArray((err, docs) => {
+    //         db.close()
+    //         if (err) {
+    //             throw Error(err)
+    //         }
+    //         recipesFromDb = docs
+    //         console.log('mongo done')
+    //     })
+    // })
+    return gulp.src('src/html/_mongo.pug')
+    .pipe(gulpData(function(file, cb) {
+      MongoClient.connect('mongodb://127.0.0.1:27017/foodsys1', function(err, db) {
+        if(err) return cb(err);
+        // cb(undefined, db.collection('recipes').findOne({name: 'Ale Bread'}));
+        db.collection('recipes').findOne(cb);
+      });
+    }))
+    .pipe(pug())
+    .pipe(gulp.dest('dist'));
+})
 
 /* Supporting tasks
 ---------------------------------------------------------------- */
@@ -34,6 +66,18 @@ gulp.task('html', () => {
         }))
         .pipe(gulp.dest('dist'))
 })
+
+gulp.task('mongo', function() {
+    console.log('recipesFromDb.length', recipesFromDb.length)
+    let opts = {
+        data: {
+            recipes: recipesFromDb
+        }
+    }
+    return gulp.src('src/html/_mongo.pug')
+        .pipe(pug(opts))
+        .pipe(gulp.dest('build'));
+});
 
 gulp.task('css', () => {
     return gulp.src('src/styles/main.sass')
