bb39f4427ce47e7b4b8e5476111aac57f4df662d
Chad Altemose <caltemose@hugeinc.com>
2017-07-24 14:49:59 -0400

    added templates task to test processing json from mongo into recipe pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp')
 const fs = require('fs')
+const path = require('path')
 const del = require('del')
 const gulpData = require('gulp-data')
+const foreach = require('gulp-foreach')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
@@ -9,7 +11,6 @@ const browsersync = require('browser-sync').create()
 const sasslint = require('gulp-sass-lint')
 const cleancss = require('gulp-clean-css')
 const rename = require('gulp-rename')
-const MongoClient = require('mongodb').MongoClient
 
 /* Helper functions
 ---------------------------------------------------------------- */
@@ -18,36 +19,10 @@ const MongoClient = require('mongodb').MongoClient
 //     return JSON.parse(fs.readFileSync('src/data/recipes.json', 'utf-8'))
 // }
 
-var recipesFromDb
-console.log('pre mongo code')
-gulp.task('db', () => {
-    // MongoClient.connect('mongodb://127.0.0.1:27017/foodsys1', function(err, db) {
-    //     if (err) {
-    //         db.close()
-    //         return err
-    //     }
+function getJsonData (file) {
+    return require(file.path)
+}
 
-    //     // cb(undefined, db.collection('recipes').find({}));
-    //     db.collection('recipes').find().toArray((err, docs) => {
-    //         db.close()
-    //         if (err) {
-    //             throw Error(err)
-    //         }
-    //         recipesFromDb = docs
-    //         console.log('mongo done')
-    //     })
-    // })
-    return gulp.src('src/html/_mongo.pug')
-    .pipe(gulpData(function(file, cb) {
-      MongoClient.connect('mongodb://127.0.0.1:27017/foodsys1', function(err, db) {
-        if(err) return cb(err);
-        // cb(undefined, db.collection('recipes').findOne({name: 'Ale Bread'}));
-        db.collection('recipes').findOne(cb);
-      });
-    }))
-    .pipe(pug())
-    .pipe(gulp.dest('dist'));
-})
 
 /* Supporting tasks
 ---------------------------------------------------------------- */
@@ -66,18 +41,6 @@ gulp.task('html', () => {
         }))
         .pipe(gulp.dest('dist'))
 })
-
-gulp.task('mongo', function() {
-    console.log('recipesFromDb.length', recipesFromDb.length)
-    let opts = {
-        data: {
-            recipes: recipesFromDb
-        }
-    }
-    return gulp.src('src/html/_mongo.pug')
-        .pipe(pug(opts))
-        .pipe(gulp.dest('build'));
-});
 
 gulp.task('css', () => {
     return gulp.src('src/styles/main.sass')
@@ -101,6 +64,22 @@ gulp.task('serve', () => {
 gulp.task('reload', (done) => {
     browsersync.reload()
     done()
+})
+
+gulp.task('templates', function() {
+    return gulp.src('data/*.json')
+        .pipe(foreach(function (stream, file) {
+            var jsonFile = file
+            var jsonBasename = path.basename(jsonFile.path, path.extname(jsonFile.path))
+            return gulp.src('src/html/test/*.pug')
+                .pipe(gulpData(getJsonData(jsonFile)))
+                .pipe(pug())
+                .pipe(rename(function(htmlFile) {
+                    htmlFile.basename = jsonBasename
+                }))
+            .pipe(gulp.dest('test'))
+        })
+    )
 })
 
 
